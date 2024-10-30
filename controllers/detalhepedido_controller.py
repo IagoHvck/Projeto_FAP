21ec72b25e2bb65b86268e1d3066d85caadcb6ca
IAGO DE OLIVEIRA ALMEIDA <IAGO0492@IOT>
2024-10-30 15:27:19 -0300

    atualização2.0

--- controllers/detalhepedido_controller.py
+++ controllers/detalhepedido_controller.py
@@ -1,15 +1,33 @@
 from flask import Blueprint, request, jsonify
-from models import db, Pedido, Produto, DetalhePedido
+from models import db, DetalhePedido
 
 detalhePedido_bp = Blueprint('detalhepedidos', __name__)
 
 @detalhePedido_bp.route('/detalhepedidos', methods=['POST'])
-def criar_detalhe_pedidos():
-
-    detalhepedido = request.json
-    novo_detalhe_pedido = DetalhePedido(dp_quantidade=detalhepedido['dp_quantidade'], dp_preco=detalhepedido['dp_preco'], dp_desconto=detalhepedido['dp_desconto'], dp_pedido_id=detalhepedido['dp_pedido_id'], dp_produto_id=detalhepedido['dp_produto_id'])
+def criar_detalhe_pedido():
+    dados = request.json
+    
+    novo_detalhe_pedido = DetalhePedido(
+        dt_pedido_id=dados['dt_pedido_id'],
+        dt_produto_id=dados['dt_produto_id'],
+        dt_valor=dados['dt_valor'],
+        dt_desconto=dados.get('dt_desconto', 0)
+    )
+    
     db.session.add(novo_detalhe_pedido)
     db.session.commit()
+    
+    return jsonify({'id': novo_detalhe_pedido.dt_id}), 201
 
-    return jsonify({'id': novo_detalhe_pedido.dp_id})
-
+@detalhePedido_bp.route('/detalhepedidos', methods=['GET'])
+def listar_detalhes_pedidos():
+    detalhes = DetalhePedido.query.all()
+    detalhes_lista = [{
+        'id': d.dt_id,
+        'pedido_id': d.dt_pedido_id,
+        'produto_id': d.dt_produto_id,
+        'valor': str(d.dt_valor),
+        'desconto': str(d.dt_desconto)  
+    } for d in detalhes]
+    
+    return jsonify(detalhes_lista), 200
